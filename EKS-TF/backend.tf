7f3941d76a40448e1ab3e8796f02c7da9e57cd5b
Oni Emmanuel <44571157+emmaxyz90@users.noreply.github.com>
2024-09-02 22:01:01 +0100

    Update backend.tf for EKS TF script

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "emmadevsecopsproj"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "devsecopsDB-emmanuel"
+    dynamodb_table = "EKS-TFDB"
     encrypt        = true
   }
   required_version = ">=0.13.0"
